314069b1326de621d6ad780be648c296867fc829
tct123 <42028373+tct123@users.noreply.github.com>
2022-10-31 06:22:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDFloatingActionButton
+from kivymd.uix.toolbar import MDTopAppBar
 
 class MainApp(MDApp):
     def build(self):
         scr = MDScreen()
         self.lbl =MDLabel()
+        toolbar = MDTopAppBar()
+        toolbar.pos_hint = {"top":1}
+        scr.add_widget(toolbar)
         self.count = 0
         self.lbl.text = f"You pressed this Button {self.count} times"
+        self.lbl.halign = "center"
         scr.add_widget(self.lbl)
         addbtn = MDFloatingActionButton(icon = "android", on_release = self.btncounter)
         scr.add_widget(addbtn)
